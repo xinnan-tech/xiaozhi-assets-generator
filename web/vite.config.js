028047a653f150e0130adad01b72745cc5f03b2a
zhuoqinglian <1035449612@qq.com>
2026-01-27 09:55:16 +0800

    fix: 修复项目访问地址

--- web/vite.config.js
+++ web/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/xiaozhi': {
-        target: 'http://test.aiot.xin-nan.com',
+        target: 'http://127.0.0.1:8002',
         changeOrigin: true
       }
     }
